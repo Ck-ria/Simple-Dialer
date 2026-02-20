7e0065133c57593865cdf5f681fea9a96b18b6e6
Ck-ria <93004396+Ck-ria@users.noreply.github.com>
2026-02-21 04:50:58 +0530

    Update build.gradle.kts
    built

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinSerialization).apply(false)
 }
+
+repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
