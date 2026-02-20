ff880c552f681ff482ecdcd89b1db490af28833a
Ck-ria <93004396+Ck-ria@users.noreply.github.com>
2026-02-21 05:04:01 +0530

    Update build.gradle.kts
    hhzj

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,12 @@ repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
